43834f0bc74a76d8bd64d9f1b1bf2a8060798db9
lukovsk1 <lukas@bosshart.dev>
2022-03-11 14:03:12 +0100

    kv-store test commit 1

---
+++ keyvaluestore/keyvaluestore_test.go
@@ -0,0 +1,13 @@
+package keyvaluestore
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSimpleInsertAndGet(t *testing.T) {
+	assert := assert.New(t)
+	KeyValueStoreManager := KeyValueStoreManager{}
+	
+}
